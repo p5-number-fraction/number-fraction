eba3cf4f88bb07e67452f7288d6afb1f10cce88c
Dave Cross <dave@dave.org.uk>
2008-03-08 13:21:31 +0000

    Changed email address and copyright date

--- lib/Number/Fraction.pm
+++ lib/Number/Fraction.pm
@@ -395,11 +395,11 @@ perldoc overload
 
 =head1 AUTHOR
 
-Dave Cross, E<lt>dave@dave.org.ukE<gt>
+Dave Cross, E<lt>dave@mag-sol.comE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2002 by Dave Cross
+Copyright 2002-8 by Dave Cross
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
